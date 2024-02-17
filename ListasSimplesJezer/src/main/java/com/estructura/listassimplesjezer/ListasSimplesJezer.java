2d0a1b5c1060aa1f082fc621405579f1a3cb380b
Eli-3strada <125108357+Eli-3strada@users.noreply.github.com>
2024-02-17 10:32:00 -0600

    Practicando listas simples
    Crearemos listas simples

---
+++ ListasSimplesJezer/src/main/java/com/estructura/listassimplesjezer/ListasSimplesJezer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.estructura.listassimplesjezer;
+
+/**
+ *
+ * @author JEZER ESTRADA
+ */
+public class ListasSimplesJezer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
