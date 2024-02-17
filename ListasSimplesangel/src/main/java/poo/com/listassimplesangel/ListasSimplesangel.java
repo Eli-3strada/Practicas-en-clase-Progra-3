bbbc86768be2599f9381e1990cce959ff66c24cb
slendy777 <125114499+slendy777@users.noreply.github.com>
2024-02-17 10:35:50 -0600

    Trabajo de java
    Proyecto trabajado en clase

---
+++ ListasSimplesangel/src/main/java/poo/com/listassimplesangel/ListasSimplesangel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.com.listassimplesangel;
+
+/**
+ *
+ * @author HP
+ */
+public class ListasSimplesangel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
