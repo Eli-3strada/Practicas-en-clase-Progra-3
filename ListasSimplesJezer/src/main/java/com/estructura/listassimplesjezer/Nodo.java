2335d4ed6b0c3091c7bfb6920583877cf8e6e9e0
Eli-3strada <125108357+Eli-3strada@users.noreply.github.com>
2024-02-22 18:36:02 -0600

    Cree una lista simplemente enlazada
    Se creo una lista simple enlazada en Netbeans

---
+++ ListasSimplesJezer/src/main/java/com/estructura/listassimplesjezer/Nodo.java
@@ -0,0 +1,19 @@
+
+package com.estructura.listassimplesjezer;
+
+/**
+ *
+ * @author JEZER ESTRADA
+ */
+public class Nodo {
+    
+    int dato; //almacena el valor del nodo
+    Nodo siguiente; //referencia al siguiente nodo de la lista
+    
+    //constructor para crear un nuevo nodo con el valor proporcionado
+    public Nodo(int dato){
+        this.dato = dato;
+        this.siguiente = null; //inicializa el siguiente nodo como nulo
+    }
+    
+}
