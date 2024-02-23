bed992516296eacda915008f975a588347e64c4d
slendy777 <125114499+slendy777@users.noreply.github.com>
2024-02-22 20:20:31 -0600

    Traspaso de código
    Código en pareja

---
+++ ListasSimplesangel/src/main/java/poo/com/listassimplesangel/Nodo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo.com.listassimplesangel;
+
+/**
+ *
+ * @author HP
+ */
+public class Nodo {
+    int dato;
+    Nodo siguiente; //referencia al siguiente nodo en la lista 
+    
+    //Constructor
+    public Nodo(int dato) {
+        this.dato=dato;//inicializamos el nodo
+        this.siguiente=null;//inicializamos el siguiente nodo como nulo 
+        
+    
+    }
+    
+}
